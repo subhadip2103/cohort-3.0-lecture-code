7ee705b53e0a264179b1e0f1754ad2fac84a1cf9
Subhodip Ghosal <s35593106@gmail.com>
2025-09-28 16:31:24 +0000

    React.memo introduced and used , explaining how it avoids that extra re-render

--- week-11.2-Recoil/src/App.jsx
+++ week-11.2-Recoil/src/App.jsx
@@ -1,43 +1,80 @@
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil"
 import { counterAtom } from "./store/atoms/counter"
+import { useState, memo, useEffect } from "react"
 
+
+// function App() {
+//   return (
+//     <RecoilRoot>
+//       <Counter/>
+//     </RecoilRoot>
+//   )
+// }
+
+// function Counter(){
+//   return(
+//     <div>
+//       <Value/>
+//       <Increase/>
+//       <Decrease/>
+//     </div>
+//   ) 
+// }
+
+// function Value(){
+//   const count=useRecoilValue(counterAtom);
+//   return <div>{count}</div>
+// }
+// function Increase(){
+//   const setCount=useSetRecoilState(counterAtom)
+//   function increase(){
+//     setCount(c=>c+1)
+//   }
+//   return <button onClick={increase}>Increase</button>
+// }
+// function Decrease(){
+//   const setCount=useSetRecoilState(counterAtom)
+//   function decrease(){
+//     setCount(c=>c-1)
+//   }
+//   return <button onClick={decrease}>Decrease</button>
+// }
+// export default App
+
+
+// Memo based approach
+
+/*React says everytime a component Re-renders all its  childrens will also re-render.
+But if u wrap a component inside a memo only if the props/state in that child has changed only then it will re-render */
 
 function App() {
-  return (
-    <RecoilRoot>
-      <Counter/>
-    </RecoilRoot>
-  )
+  return <Counter />
+
 }
 
-function Counter(){
-  return(
-    <div>
-      <Value/>
-      <Increase/>
-      <Decrease/>
-    </div>
-  ) 
-}
+function Counter() {
+  const [count, setCount] = useState(0);
 
-function Value(){
-  const count=useRecoilValue(counterAtom);
-  return <div>{count}</div>
+  useEffect(() => {
+    setInterval(() => {
+      setCount(c => c + 1)
+    }, 3000);
+  }, [])
+  return <div>
+    <Value />
+    <Increase />
+    <Decrease />
+  </div>
+
 }
-function Increase(){
-  const setCount=useSetRecoilState(counterAtom)
-  function increase(){
-    setCount(c=>c+1)
-  }
-  return <button onClick={increase}>Increase</button>
-}
-function Decrease(){
-  const setCount=useSetRecoilState(counterAtom)
-  function decrease(){
-    setCount(c=>c-1)
-  }
-  return <button onClick={decrease}>Decrease</button>
-}
+const Value = memo(function () {
+  return <div>1</div>
+})
+const Increase = memo(function () {
+  return <button>Increase</button>
+})
+const Decrease = memo(function () {
+  return <button>Decrease</button>
+})
+
 export default App
-
-
