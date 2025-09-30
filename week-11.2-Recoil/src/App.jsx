2be3c3ace4b9bdbf69cf71d34689775170c7cfd6
Subhodip Ghosal <s35593106@gmail.com>
2025-09-30 06:25:18 +0000

    Add selectors in recoil and used using a counter example

--- week-11.2-Recoil/src/App.jsx
+++ week-11.2-Recoil/src/App.jsx
@@ -1,6 +1,41 @@
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil"
-import { counterAtom } from "./store/atoms/counter"
+import { counterAtom, evenSelector } from "./store/atoms/counter"
 import { useState, memo, useEffect } from "react"
+
+
+function App() {
+  return <RecoilRoot>
+    <Buttons />
+    <Counter />
+    <IsEven />
+  </RecoilRoot>
+
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(counterAtom);
+  function increase() {
+    setCount(c => c + 2);
+  }
+  function decrease() {
+    setCount(c => c - 1)
+  }
+  return (
+    <div>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+    </div>
+  )
+}
+function Counter() {
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>
+}
+function IsEven() {
+  const even = useRecoilValue(evenSelector);
+  return <div>{even ? "Even":"Odd"}</div>
+}
+
 
 
 // function App() {
@@ -39,7 +74,7 @@ import { useState, memo, useEffect } from "react"
 //   }
 //   return <button onClick={decrease}>Decrease</button>
 // }
-// export default App
+export default App
 
 
 // Memo based approach
@@ -47,34 +82,34 @@ import { useState, memo, useEffect } from "react"
 /*React says everytime a component Re-renders all its  childrens will also re-render.
 But if u wrap a component inside a memo only if the props/state in that child has changed only then it will re-render */
 
-function App() {
-  return <Counter />
+// function App() {
+//   return <Counter />
 
-}
+// }
 
-function Counter() {
-  const [count, setCount] = useState(0);
+// function Counter() {
+//   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    setInterval(() => {
-      setCount(c => c + 1)
-    }, 3000);
-  }, [])
-  return <div>
-    <Value />
-    <Increase />
-    <Decrease />
-  </div>
+//   useEffect(() => {
+//     setInterval(() => {
+//       setCount(c => c + 1)
+//     }, 3000);
+//   }, [])
+//   return <div>
+//     <Value />
+//     <Increase />
+//     <Decrease />
+//   </div>
 
-}
-const Value = memo(function () {
-  return <div>1</div>
-})
-const Increase = memo(function () {
-  return <button>Increase</button>
-})
-const Decrease = memo(function () {
-  return <button>Decrease</button>
-})
+// }
+// const Value = memo(function () {
+//   return <div>1</div>
+// })
+// const Increase = memo(function () {
+//   return <button>Increase</button>
+// })
+// const Decrease = memo(function () {
+//   return <button>Decrease</button>
+// })
 
-export default App
+// export default App
--- week-11.2-Recoil/src/store/atoms/counter.jsx
+++ week-11.2-Recoil/src/store/atoms/counter.jsx
@@ -1,6 +1,14 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const counterAtom = atom({
     key: "Counter",
     default: 0
+})
+
+export const evenSelector = selector({
+    key: "isEvenSelector",
+    get: ({ get }) => {
+        const currentCount = get(counterAtom);
+        return currentCount % 2 === 0;
+    }
 })
