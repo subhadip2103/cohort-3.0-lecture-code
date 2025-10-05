786005efba73e8d3a7a333fa36d82da8dbde3e08
Subhodip Ghosal <s35593106@gmail.com>
2025-10-05 15:37:44 +0000

    Changed file structure and added atom family code and used it

---
+++ week-11.3-Recoil Deep Dive/Atom Family, Selector Family/src/App.jsx
@@ -0,0 +1,27 @@
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import { todosAtomFamily } from './store/atom';
+
+function App() {
+  return <RecoilRoot>
+    <Todo id={1}/>
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={1} />
+    <Todo id={1} />
+    <Todo id={2} />
+  </RecoilRoot>
+}
+
+function Todo({id}) {
+   const todo = useRecoilValue(todosAtomFamily(id));
+
+  return (
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
+  )
+}
+
+export default App
